24ab6c87757d957d22ca0917bf1e52e591e8a000
francoa.taffarel <francoa.taffarel@gmail.com>
2023-09-19 07:01:21 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,16 @@
-run.sh
+#!/bin/bash
+
+# Ler os comandos do arquivo docker-run-check-vulns.txt
+commands=()
+while IFS= read -r cmd
+do
+    commands+=("$cmd")
+done < docker-run-check-vulns.txt
+
+# Loop para executar os comandos em sessões separadas do screen
+for cmd in "${commands[@]}"
+do
+  echo $cmd
+  sleep 60;sudo  screen -dmS docker_session bash -c "$cmd"
+done
+
